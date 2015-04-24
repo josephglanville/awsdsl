622ffa814285adc286fd5f7996eaeae354701824
Joseph Glanville <jpg@jpg.id.au>
2015-04-25 04:05:03 +1000

    Further flesh out command line

--- lib/awsdsl/command_line.rb
+++ lib/awsdsl/command_line.rb
@@ -3,22 +3,38 @@ require 'awsdsl'
 
 module AWSDSL
   class CommandLine < Clamp::Command
-    option ['-s', '--stackfile'],
+    option '--stackfile',
       'STACKFILE',
       'Path to Stackfile',
       default: 'Stackfile'
-    option ['-b', '--build'], :flag, 'Build AMIs'
 
-    def execute
-      stack = Loader.load(stackfile)
-      if build_amis
+    subcommand 'build', 'Build Stack AMIs' do
+      def execute
+        stack = Loader.load(stackfile)
         AMIBuilder.build(stack)
-      else
-        # Get the latest AMIs
       end
-      template = CfnBuilder.build(stack)
-      cfm = AWS::CloudFormation.new
-      # Check state of stack, create or update as necessary
+    end
+
+    subcommand 'create', 'Create Stack' do
+      def execute
+        stack = Loader.load(stackfile)
+        # update stack with AMIs
+        template = CfnBuilder.build(stack)
+        cfm = AWS::CloudFormation.new
+        cfm.stacks.create(stack.name.capitalize, template,
+                          capabilities: ['CAPABILITY_IAM'])
+      end
+    end
+
+    subcommand 'update', 'Update Stack' do
+      def execute
+        stack = Loader.load(stackfile)
+        # Update stack with AMIs
+        template = CfnBuilder.build(stack)
+        cfm = AWS::CloudFormation.new
+        cfm.stacks[stack.name.capitalize].update(template: template,
+                                                 capabilities: ['CAPABILITY_IAM'])
+      end
     end
   end
 end
